fd66a8b32c8c1e68fb37cb872f162ab29a6cb122
Kipras Lisauskas <kipraslisauskas@gmail.com>
2025-12-23 00:40:25 +0200

    WIP DeskCard on hover logic

--- frontend/src/components/Desk/DeskCard.jsx
+++ frontend/src/components/Desk/DeskCard.jsx
@@ -18,23 +18,34 @@ const DeskCard = ({ desk, user, fromDate, toDate }) => {
     const occupiedByUserOther = occupyingReservations.some((r) => r.userId !== user.id);
     const occupiedByUserSelf = occupyingReservations.some((r) => r.userId === user.id);
 
+    const reservedUser = occupyingReservations.find((r) => r.userId !== user.id);
+    if (reservedUser) console.log(reservedUser);
+
     let deskStatus = "available";
 
-        if (occupiedByUserOther) {
-            deskStatus = "occupied-other";
-        }
-        else if (occupiedByUserSelf) {
-            deskStatus = "occupied-self";
-        }
+    if (occupiedByUserOther) {
+        deskStatus = "occupied-other";
+    } else if (occupiedByUserSelf) {
+        deskStatus = "occupied-self";
+    }
 
     return (
         <div className="desk-card" onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut}>
             <p>{desk.number}</p>
             {occupiedByUserSelf && <p>Occupied by self</p>}
             {occupiedByUserOther && <p>Occupied by Other</p>}
-            {isHovered && <p>Hovered</p>}
+            {isHovered && deskStatus === "occupied-self" && <CancelButton />}
+            {isHovered && deskStatus === "occupied-other" && (
+                <p>
+                    Reserved by: {reservedUser.firstName} {reservedUser.lastName}
+                </p>
+            )}
         </div>
     );
 };
 
 export default DeskCard;
+
+const CancelButton = () => {
+    return <button>Cancel</button>;
+};
