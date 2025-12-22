4e1c204a87efb7537752fe86f3247afd2db7c0f0
Kipras Lisauskas <kipraslisauskas@gmail.com>
2025-12-22 23:32:07 +0200

    Handle conditional hovering on table cards

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     const [fromDate, setFromDate] = useState(getFormattedDefaultDate());
     const [desks, setDesks] = useState([]);
     const [user, setUser] = useState(null);
-    const [loadingData, setLoadingData] = useState(true);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const loadData = async () => {
@@ -21,7 +21,7 @@ const Home = () => {
                     .then((res) => res.json())
                     .then((data) => setDesks(data)),
             ]);
-            setLoadingData(false);
+            setLoading(false);
         };
 
         loadData();
@@ -34,19 +34,27 @@ const Home = () => {
         console.log("To Date:", toDate);
     }
 
-    if (loadingData) return <h1>Loading...</h1>;
+    if (loading) return <h1>Loading...</h1>;
 
     return (
         <div>
             <form id="date-range-form" onSubmit={handleSubmit}>
                 <h2>Select reservation date range:</h2>
                 <DatePicker
+                    id="from-date"
                     label="From:"
                     date={fromDate}
                     setDate={setFromDate}
                     minDate={getFormattedDefaultDate()}
+                    maxDate={toDate}
                 />
-                <DatePicker label="To:" date={toDate} setDate={setToDate} minDate={fromDate} />
+                <DatePicker
+                    id="to-date"
+                    label="To:"
+                    date={toDate}
+                    setDate={setToDate}
+                    minDate={fromDate}
+                />
                 <button type="submit">See availability</button>
             </form>
 
@@ -55,13 +63,15 @@ const Home = () => {
     );
 };
 
-const DatePicker = ({ label, date, setDate, minDate }) => {
+const DatePicker = ({ id, label, date, setDate, minDate, maxDate }) => {
     return (
         <>
             <label htmlFor="from-date">{label}</label>
             <input
+                id={id}
                 type="date"
                 min={minDate}
+                max={maxDate}
                 value={date}
                 onChange={(e) => setDate(e.target.value)}
             />
@@ -70,12 +80,38 @@ const DatePicker = ({ label, date, setDate, minDate }) => {
 };
 
 const DeskCard = ({ desk, user, fromDate, toDate }) => {
-    const isOccupied = desk.reservations.some((r) => r.startDate < toDate && r.endDate > fromDate);
+    const [isHovered, setIsHovered] = useState(false);
+
+    function handleMouseOver() {
+        setIsHovered(true);
+    }
+
+    function handleMouseOut() {
+        setIsHovered(false);
+    }
+
+    const isOccupied = (reservation) =>
+        reservation.startDate < toDate && reservation.endDate > fromDate;
+
+    const occupyingReservations = desk.reservations.filter((r) => isOccupied(r));
+    const occupiedByUserOther = occupyingReservations.some((r) => r.userId !== user.id);
+    const occupiedByUserSelf = occupyingReservations.some((r) => r.userId === user.id);
+
+    let deskStatus = "available";
+
+        if (occupiedByUserOther) {
+            deskStatus = "occupied-other";
+        }
+        else if (occupiedByUserSelf) {
+            deskStatus = "occupied-self";
+        }
 
     return (
-        <div className="desk-card">
+        <div className="desk-card" onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut}>
             <p>{desk.number}</p>
-            {isOccupied && <p>Occupied</p>}
+            {occupiedByUserSelf && <p>Occupied by self</p>}
+            {occupiedByUserOther && <p>Occupied by Other</p>}
+            {isHovered && <p>Hovered</p>}
         </div>
     );
 };
